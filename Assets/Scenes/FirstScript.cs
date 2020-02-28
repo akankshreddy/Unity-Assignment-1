c173dc1591e6f8f9906434a9f7e35c2267529304
KIRA <KIRA@MacBook-Pro.local>
2020-02-28 17:39:53 +0530

    Unity Ass 1

---
+++ Assets/Scenes/FirstScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FirstScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadScene(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+
+    // Update is called once per frame
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
